dc239fed13483e1e052842786139d81057987e0b
Michał Mroczkowski <mchlmroczkowski@gmail.com>
2021-03-24 19:12:24 +0100

    Add main2.py

---
+++ main2.py
@@ -0,0 +1,11 @@
+shopping_dict = {
+    "warzywniak": ['marchew', 'seler', 'rukola'],
+    "piekarnia": ['chleb', 'pączek', 'bułki']}
+k=0
+print('Lista zakupów')
+for r in shopping_dict :
+     k = k + len(shopping_dict[r])
+    print(f'Idę do {str.capitalize(r)} kupić {shopping_dict[r]}')
+print(f'Kupiono {k} produktów')   
+print('Nie kupiono sera i jabłek')
+
